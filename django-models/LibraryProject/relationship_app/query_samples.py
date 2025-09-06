f8247a3960816509accaaf97f0457ef99dd1e262
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-06 16:47:57 +0100

    engaging checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,7 @@
 author = Book.author.filter(name="George Orwell")
-a1 = Author.objects.filter(name=author_name)
+a1 = Author.objects.get(name=author_name)
+#objects.filter(author=author)
+a1.objects.filter(author=author)
 
 books = Library.books.all()
 b1 = Library.objects.get(name=library_name)
