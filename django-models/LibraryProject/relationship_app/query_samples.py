d00a1ea27b2461a871942e521918ceabbf9a59d2
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-06 13:06:27 +0100

    checkker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,7 @@
 author = Book.author.filter(name="George Orwell")
+a1 = Author.objects.filter(name=author_name)
 
 books = Library.books.all()
-
+b1 = Library.objects.get(name=library_name)
 librarian = Librarian.library.get(name="LBS")
+
