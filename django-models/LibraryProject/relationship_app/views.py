8d5f16ecd1b644c937fa75db1451358a0f02df58
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-07 06:22:04 +0100

    i am in way over my head

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from djang.urls import reverse_lazy
 from django.http import HttpResponseForbidden
-from django.contib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -40,7 +41,7 @@ class RegisterView(CreateView):
 """from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 
-# helper decorator
+ helper decorator
 def role_required(allowed_roles=[]):
     def decorator(view_func):
         def wrapper(request, *args, **kwargs):
@@ -68,8 +69,8 @@ def member_view(request):
     return render(request, "member_page.html", {"user": request.user})"""
 #-------------------------------------------------####
 
-
-
+#------------THIS SECTION USES THE SIMPLIFIED VERSION OF THE ABOVE LOGIC
+"""
 @login_required
 def admin_view(request):
     if request.user.userprofile.role == "Admin":
@@ -101,3 +102,33 @@ def signup_view(request):
         logn(request, user)
         return redirect("library")
     return render(request, "register.html")
+
+"""
+
+# Helpers
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
