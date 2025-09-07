6da91b3f854135eb74259349497d8b85a0426304
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-07 04:51:58 +0100

    next

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView, ListView
-from .models import Book, Library, Author
+from .models import Book
+from .models import Library
 # Create your views here.
 
 def book_list(request):
@@ -12,7 +13,8 @@ def book_list(request):
 
 class Library(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    #'relationship_app.library_detail.html'
+    template_name = "relationship_app/library_detail.html"
 
     def get_context_data(Self, **kwargs):
         context = super().get_context_data(**kwargs)
