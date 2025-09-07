ac866d494ca3144e61e5fe87a160fd2642acfbb4
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-07 21:38:43 +0100

    4

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin_view/', views.admin_view, name='admin_view'),
+    path('admin_view/', views.Admin_view, name='admin_view'),
     path('librarian_view/', views.Librarian_view, name='librarian_view'),
     path('member_view/', views.Member_view, name='member_view'),
+    path('book/add/', views.book_add, name='book_add'),
+    path('book/<int:pk>/edit/', views.book_edit, name='book_edit'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.http import HttpResponseForbidden
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, DetailView
 from .models import Book, Library, UserProfile
+from django import forms
 
 # Create your views here.
 
@@ -47,3 +48,39 @@ def Librarian_view(request):
 @user_passes_test(is_member)
 def Member_view(request):
     return render(request, 'relationship_app/member_page.html')
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
+
+@permission_required('relationship_app.can_add_book')
+def book_add(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/book_form.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book')
+def book_edit(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/book_form.html', {'form': form})
+
+@permission_required('relationship_app.can_delete_book')
+def book_delete(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('books')
+    return render(request, 'relationship_app/book_confirm_delete.html', {'object': book})
