c5c4734f43fbd8885e6ee988a5cfc993a7b6566d
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-07 22:49:38 +0100

    3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views.admin_view import admin_view
+from .views.librarian_view import librarian_view
+from .views.member_view import member_view
 from . import views
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
@@ -9,9 +12,9 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin_view/', views.Admin_view, name='admin_view'),
-    path('librarian_view/', views.Librarian_view, name='librarian_view'),
-    path('member_view/', views.Member_view, name='member_view'),
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
     path('book/add_book/', views.add_book, name='book_add'),
     path('book/<int:pk>/edit_book/', views.edit_book, name='book_edit'),
     path('book/<int:pk>/delete_book/', views.delete_book, name='book_delete'),
