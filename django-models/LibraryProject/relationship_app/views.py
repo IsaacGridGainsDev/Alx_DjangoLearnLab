8e0dc862972e39593b634825c5b3b080d42c8a10
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-07 21:40:55 +0100

    YES

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -36,7 +36,7 @@ def is_member(user):
 # Views
 @login_required
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin_view(request):
     return render(request, 'relationship_app/admin_page.html')
 
 @login_required
