9942ec770b4eb9c417a4b2a77a980fd48d6d5d7f
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-14 12:37:55 +0100

    task 0

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
@@ -8,3 +9,9 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author')
     search_fields = ('title', 'author')
 admin.site.register(Book)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Addotional Info", {'fields': ('date_of_birth', 'profile_photo')}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Addotional Info", {'fields': ('date_of_birth', 'profile_photo')}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
