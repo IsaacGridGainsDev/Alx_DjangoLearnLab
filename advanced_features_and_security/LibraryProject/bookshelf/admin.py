3c310c6f0885f4970d7705316b25224090eb35f2
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-14 12:41:52 +0100

    task 1

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Book, CustomUser
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group, Permission
 # Register your models here.
+
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -15,3 +17,12 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (("Addotional Info", {'fields': ('date_of_birth', 'profile_photo')}),)
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+editors, created = Group.objects.get_or_create(name='Editors')
+viewers, created = Group.objects.get_or_create(name='Viewers')
+
+editors.permissions.add(Permission.objects.get(codename='can_add_book'))
+editors.permissions.add(Permission.objects.get(codename='can_change_book'))
+editors.permissions.add(Permission.objects.get(codename='can_delete_book'))
+
+viewers.permissions.add(Permission.objects.get(codename='can_view_book'))
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import permission_required
 # Create your views here.
+
+@permission_required('bookshelf.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form})
