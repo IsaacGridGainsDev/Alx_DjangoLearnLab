10a27cf7db362af74eaf0ce0d65ed421952a95ef
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-21 14:03:16 +0100

    For God is able

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api_project.api_project.urls import urlpatterns
 from .views import BookList, BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='books_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('auth/', obtain_auth_token, name='api-token-auth'),
 ]
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -54,6 +54,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'api_project.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -123,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
