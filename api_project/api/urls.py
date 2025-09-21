796785cfebef4109c6f78240c6875e66289abbb0
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-21 13:51:18 +0100

    Task 2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from api_project.api_project.urls import urlpatterns
-from .views import BookList
+from .views import BookList, BookViewSet
 
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='books_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
