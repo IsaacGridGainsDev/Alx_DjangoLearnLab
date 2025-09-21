981f0aa846e840f62419cd7b8fcaa44eec5c4ee2
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-21 13:52:15 +0100

    finale

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,6 +8,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSer(viewsets.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
