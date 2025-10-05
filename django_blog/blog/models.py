aad5f1b45966b4c9d0e1925dbaa009f5e4bb8873
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-10-05 23:34:59 +0100

    \SDAS

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,20 +1,28 @@
+# blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
-# from taggit.managers import TaggableManager
 from django.urls import reverse
 
 class Post(models.Model):
+    """
+    Post model - stores blog post content.
+    Fields:
+      - title: short title
+      - content: full body
+      - published_date: auto set when created
+      - author: FK to User
+    """
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    # tags = TaggableManager(blank=True)  # django-taggit
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
     class Meta:
-        ordering = ['-published_date']
+        ordering = ["-published_date"]
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post-detail', args=[self.pk])
+        # used by CreateView/UpdateView to redirect to detail
+        return reverse("post-detail", args=[str(self.pk)])
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,18 @@
 # blog/urls.py
 from django.urls import path
-from .views import UserLoginView, UserLogoutView, RegisterView, profile_view
+from .views import (
+    PostListView, PostDetailView, PostCreateView,
+    PostUpdateView, PostDeleteView,
+    # ... (other auth views)
+)
 
 urlpatterns = [
-    path("login/", UserLoginView.as_view(), name="login"),
-    path("logout/", UserLogoutView.as_view(), name="logout"),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("profile/", profile_view, name="profile"),
+    # Post CRUD
+    path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/new/", PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+
+    # keep other URLs (auth etc.) here...
 ]
